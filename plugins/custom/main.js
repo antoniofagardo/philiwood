f95471a0bd7ed404a7118545adb6f8662af279f3
antoniofagardo <antoniofagardo@gmail.com>
2021-01-25 08:02:10 +0000

    deploy: antoniofagardo/ecommerce-wood@554e8ffe261cb7415121f586aaabd33d908759f2

---
+++ plugins/custom/main.js
@@ -0,0 +1,25 @@
+$('.contact1-form').on('submit', function (e) {
+    //optional validation code here
+    console.log("Stopping event propagation");
+    e.preventDefault();
+    console.log("Sending ajax request");
+    $.ajax({
+        url: "{{ site.Params.contact.formAction }}",
+        method: "POST",
+        dataType: "json",
+        data: $(".contact1-form").serialize(),
+        success: function (response) {
+            if (response.result == "success") {
+                $('.contact1-form')[0].reset();
+                alert('Thank you for contacting us.');
+                return true;
+            }
+            else {
+                alert("Something went wrong. Please try again.")
+            }
+        },
+        error: function () {
+            alert("Something went wrong. Please try again.")
+        }
+    })
+});
